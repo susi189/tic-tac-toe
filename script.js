8d6660106d140de95cdfb16a1d1ef0e940ac83fa
susi189 <ssteinhoff82@gmail.com>
2021-05-13 15:26:56 -0400

    Added playGame function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const boardSection = document.querySelector('.board');
 
 const gameBoard = (()=>{
-   const createBoard = () => {
+    let board = ['','','','','','','','',''];
+    const createBoard = () => {
        //the board will be an aray of divs
-    let board = ['x','o','x','x','o','x','o','x','x'];
     board.forEach(function(item){
         let elem = document.createElement('div');
         elem.innerText = item;
@@ -15,7 +15,11 @@ const gameBoard = (()=>{
    };
 })();
 
-gameBoard.createBoard();
+gameBoard.createBoard()
+
+// lastTurn -> 
+// 0 have not played
+// 1 just had a turn
 
 function Player(name, lastTurn){
     const getName = () => name;
@@ -24,6 +28,30 @@ function Player(name, lastTurn){
     return {getName, getTurn}
 }
 
-const playGame = (player1, player2) => {
+const playerO = Player('O', 0)
+const playerX = Player('X', 0)
 
+const playGame = (player) => {
+    boardSection.addEventListener('click', (event) => {
+        if(event.target.innerText === ''){
+            if(player === playerO){
+                event.target.innerText = 'O';
+                player = playerX;
+            } else if(player === playerX){
+                event.target.innerText = 'X';
+                player = playerO;
+            }
+        }
+        
+    })
 }
+
+const checkTheFields = (array) => {
+    
+}
+
+const clearFields = () => {
+    
+}
+
+playGame(playerO)
