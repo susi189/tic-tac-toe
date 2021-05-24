58e9bb4c2e9efc48148d491bd909e2a78de251cd
susi189 <ssteinhoff82@gmail.com>
2021-05-24 16:58:07 -0400

    Added index and made minor changes to the gameBoard function

--- script.js
+++ script.js
@@ -2,34 +2,34 @@ const boardSection = document.querySelector('.board');
 
 const gameBoard = (()=>{
     let board = ['','','','','','','','',''];
-    const createBoard = () => {
-       //the board will be an aray of divs
-    board.forEach(function(item){
+    const displayBoard = () => {
+    board.forEach(function(item, index){
         let elem = document.createElement('div');
         elem.innerText = item;
-        boardSection.appendChild(elem).setAttribute('id', 'element');
+        boardSection.appendChild(elem).setAttribute('class', 'element');
+        elem.setAttribute('id', index);
     })
    }
    return {
-       createBoard
+        displayBoard
    };
 })();
 
-gameBoard.createBoard()
+
+gameBoard.displayBoard();
 
 // lastTurn -> 
 // 0 have not played
 // 1 just had a turn
 
-function Player(name, lastTurn){
+function Player(name){
     const getName = () => name;
-    const getTurn = () => lastTurn;
 
-    return {getName, getTurn}
+    return {getName}
 }
 
-const playerO = Player('O', 0)
-const playerX = Player('X', 0)
+const playerO = Player('O')
+const playerX = Player('X')
 
 const playGame = (player) => {
     boardSection.addEventListener('click', (event) => {
